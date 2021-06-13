a1f92e39587defdce22b3bad8d3ebb9784c4da50
Lara <s258856@studenti.polito.it>
2021-06-13 12:13:55 +0200

    versione 1 (giusto il numero di archi e vertici del grafo)

--- src/main/java/it/polito/tdp/PremierLeague/db/PremierLeagueDAO.java
+++ src/main/java/it/polito/tdp/PremierLeague/db/PremierLeagueDAO.java
@@ -6,7 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import it.polito.tdp.PremierLeague.model.Action;
+import it.polito.tdp.PremierLeague.model.Adiacenza;
 import it.polito.tdp.PremierLeague.model.Match;
 import it.polito.tdp.PremierLeague.model.Player;
 
@@ -60,25 +63,53 @@ public class PremierLeagueDAO {
 		}
 	}
 	
-	public List<Match> listAllMatches(){
+	public void listAllMatches(Map <Integer, Match> idMap){
 		String sql = "SELECT m.MatchID, m.TeamHomeID, m.TeamAwayID, m.teamHomeFormation, m.teamAwayFormation, m.resultOfTeamHome, m.date, t1.Name, t2.Name   "
 				+ "FROM Matches m, Teams t1, Teams t2 "
 				+ "WHERE m.TeamHomeID = t1.TeamID AND m.TeamAwayID = t2.TeamID";
-		List<Match> result = new ArrayList<Match>();
+		
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
-
+					if(!idMap.containsKey(res.getInt("m.MatchID"))) {
 				
 				Match match = new Match(res.getInt("m.MatchID"), res.getInt("m.TeamHomeID"), res.getInt("m.TeamAwayID"), res.getInt("m.teamHomeFormation"), 
 							res.getInt("m.teamAwayFormation"),res.getInt("m.resultOfTeamHome"), res.getTimestamp("m.date").toLocalDateTime(), res.getString("t1.Name"),res.getString("t2.Name"));
 				
 				
-				result.add(match);
+				idMap.put(res.getInt("m.MatchID"), match);
 
+			}
+			}
+			conn.close();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public List <Match> getVertici(int mese, Map<Integer, Match> idMap){
+		String sql = "SELECT * "
+				+ "FROM matches m "
+				+ "WHERE month(m.Date) = ?";
+		Connection conn = DBConnect.getConnection();
+		List <Match> result = new ArrayList<>();
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, mese);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				if(idMap.containsKey(res.getInt("m.MatchID"))) {
+					result.add(idMap.get(res.getInt("m.MatchID"))); 
+				}
+				
 			}
 			conn.close();
 			return result;
@@ -88,5 +119,38 @@ public class PremierLeagueDAO {
 			return null;
 		}
 	}
+	
+	public List <Adiacenza> getAdiacenze(int mese, Map<Integer, Match> idMap, int minuti){
+		String sql = "SELECT m1.MatchID, m2.MatchID, a1.PlayerID, a2.PlayerID, a1.TimePlayed, a2.TimePlayed , COUNT(DISTINCT(a1.PlayerID)) AS conta "
+				+ "FROM actions a1, actions a2, matches m1, matches m2 "
+				+ "WHERE a1.TimePlayed >= ? AND a2.TimePlayed>= a1.TimePlayed AND a1.MatchID< a2.MatchID AND a1.PlayerID=a2.PlayerID "
+				+ "AND a1.MatchID=m1.MatchID AND month(m1.Date)=? AND a2.MatchID=m2.MatchID AND MONTH(m2.Date)=month(m1.Date) "
+				+ "GROUP BY m1.MatchID, m2.MatchID ";
+		Connection conn = DBConnect.getConnection();
+		List <Adiacenza> result = new ArrayList<>();
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, minuti);
+			st.setInt(2, mese);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				Match m1 = idMap.get(res.getInt("m1.MatchID"));
+				Match m2 = idMap.get(res.getInt("m2.MatchID"));
+				Adiacenza a = new Adiacenza(m1,m2,res.getInt("conta"));
+				result.add(a);
+				
+				
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 	
 }
--- src/main/java/it/polito/tdp/PremierLeague/model/Model.java
+++ src/main/java/it/polito/tdp/PremierLeague/model/Model.java
@@ -14,4 +14,30 @@ import it.polito.tdp.PremierLeague.db.PremierLeagueDAO;
 
 public class Model {
 	
+	private SimpleWeightedGraph <Match,DefaultWeightedEdge> grafo;
+	private PremierLeagueDAO dao;
+	private Map <Integer, Match> idMap;
+	
+	public Model(){
+		
+	dao = new PremierLeagueDAO();
+	idMap = new HashMap<>();
+	dao.listAllMatches(idMap);
+		
+	}
+	
+	public void creaGrafo(int mese, int minuti) {
+		grafo = new SimpleWeightedGraph <>(DefaultWeightedEdge.class);
+		//vertici
+		Graphs.addAllVertices(grafo,dao.getVertici(mese, idMap));
+		
+		//archi
+		for(Adiacenza a : dao.getAdiacenze(mese, idMap, minuti)) {
+			Graphs.addEdgeWithVertices(grafo, a.getM1(), a.getM2(), a.getPeso());
+		}
+		
+		
+		System.out.println("GRAFO CREATO!" + this.grafo.vertexSet().size()+" "+this.grafo.edgeSet().size());
+	}
+	
 }
